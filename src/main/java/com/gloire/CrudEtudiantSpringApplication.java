c69a7f58b50e6e630041a548d2773c8dda309002
Brandem Ambali <brandemambali@gmail.com>
2024-02-08 23:28:14 +0000

    Initial commit 1

---
+++ src/main/java/com/gloire/CrudEtudiantSpringApplication.java
@@ -0,0 +1,13 @@
+package com.gloire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudEtudiantSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudEtudiantSpringApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/gloire/repository/EtudiantRepository.java
@@ -0,0 +1,9 @@
+package com.gloire.repository;
+
+import com.gloire.entity.Etudiant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EtudiantRepository extends JpaRepository<Etudiant, Integer> {
+}
